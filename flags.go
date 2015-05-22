feb0bcb16779284784466dd32a62bebfc0a07ba6
Luke Evers <me@lukevers.com>
2015-05-22 09:29:13 -0400

    Put all vars in one var statement for better go formatting

--- flags.go
+++ flags.go
@@ -4,10 +4,12 @@ import (
 	"flag"
 )
 
-var path = flag.String("font-path", "fonts/itchy.ttf", "Path to font file")
-var name = flag.String("name", "out.png", "Image name output.")
-var text = flag.String("text", "Test", "The text to be displayed")
-var size = flag.Float64("size", 16, "The font size to use")
-var width = flag.Int("width", 640, "The width of the image")
-var height = flag.Int("height", 480, "The height of the image")
-var dpi = flag.Float64("dpi", 300, "The DPI of the image.")
+var (
+	path   = flag.String("font-path", "fonts/itchy.ttf", "Path to font file")
+	name   = flag.String("name", "out.png", "Image name output.")
+	text   = flag.String("text", "Test", "The text to be displayed")
+	size   = flag.Float64("size", 16, "The font size to use")
+	width  = flag.Int("width", 640, "The width of the image")
+	height = flag.Int("height", 480, "The height of the image")
+	dpi    = flag.Float64("dpi", 300, "The DPI of the image.")
+)
